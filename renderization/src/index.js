c786b40a4afa2bd63b17ae1650abaf6acc064ba0
Diego Henrique <diegohrp20@gmail.com>
2022-01-07 05:35:34 -0300

    Adicionando o projeto renderization.

---
+++ renderization/src/index.js
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react"
+import ReactDOM from "react-dom"
+import "./styles.css"
+
+const element1 = 'Prazer, me chamo Diego Henrique,'
+const element2 = <h1>E seja bem vindo a página! :)</h1>
+
+function App() {
+  return (
+    <div>
+      {element1}
+      {element2}
+    </div>
+  )
+}
+
+const rootElement = document.getElementById("root")
+ReactDOM.render(<App />, rootElement)
