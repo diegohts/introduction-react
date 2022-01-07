ab155ca0215c4d8658a33ce9cba9b53a21944109
Diego Henrique <diegohrp20@gmail.com>
2022-01-07 16:00:30 -0300

    Excluindo comentários.

--- components/src/Button.jsx
+++ components/src/Button.jsx
@@ -3,11 +3,7 @@ import React from "react";
 function Button(props) {
 
     const { name, onClick } = props
-/*
-Equivalente a linha acima, porém acaba deixando o código mais legível 
-    const name = props.name
-    const onClick = props.onClick
-*/
+    
     return (
         <button onClick={ onClick }>
             { name }
