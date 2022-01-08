c5668df97ef819d8faa40848e3700cd5f1b5bbb3
Diego Henrique <diegohrp20@gmail.com>
2022-01-08 01:58:27 -0300

    Organizando os projetos de Introdução a reactJS em modulos e acrescentando tambem o ultimo projeto que integrou o webpack ao desenvolvimento com reactJS.

---
+++ modulo2/webpack-config/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Button from './Button'
+const App = () => {
+  return (
+    <div>
+      <p>Olá, seja bem vindo!</p>
+      <p>Me chamo Diego =D.</p>
+      <div>Webpack 4</div>
+      <br />
+      <Button />
+    </div>
+  );
+};
+export default App;
---
+++ modulo2/webpack-config/src/Button.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Button = () => (
+  <button> Oi </button>
+)
+
+export default Button
